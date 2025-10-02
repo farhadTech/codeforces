cce45a844363dff34d1b06cda3598dda3853d020
Farhadur <farhad.rahmandev@gmail.com>
2025-10-02 15:06:19 +0600

    Add solution for CF :A. Be Positive - Language: C++ - Approach: Brute force

---
+++ Virtual Contest/a_be_positive.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int>arr(n);
+
+  for(int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+
+  int totalNegative = 0;
+  for(int i = 0; i < n; i++){
+    if(arr[i] == -1) {
+      totalNegative++;
+    }
+  }
+  int ops = 0;
+  for(int i = 0; i < n; i++){
+    if(arr[i] == 0) {
+      ops++;
+    }
+  }
+
+  if(totalNegative % 2 != 0) {
+    ops+=2;
+  }
+  cout << ops << "\n";
+}
+
+int main() {
+  int test_cases;
+  cin >> test_cases;
+
+  for(int i = 1; i <= test_cases; i++) {
+    solve();
+  }
+  return 0;
+}
